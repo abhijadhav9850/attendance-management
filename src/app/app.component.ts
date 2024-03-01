0f23943842f0455d8937f7a375c2886d697a5a92
rudraBelwalkar <144021775+rudraBelwalkar@users.noreply.github.com>
2024-03-01 17:51:22 +0530

    added profile component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/layouts/navbar/navbar.component';
+import { ProfileComponent } from './components/layouts/profile/profile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent],
+  imports: [RouterOutlet,NavbarComponent,ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
