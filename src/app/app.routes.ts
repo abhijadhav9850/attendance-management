512475dcdf4448320be6bbda11c3752e0f3125f5
Abhishek Jadhav <81374155+abhijadhav9850@users.noreply.github.com>
2024-03-01 15:28:00 +0530

    add some basic text

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { NavbarComponent } from './components/layouts/navbar/navbar.component';
+import { SidebarComponent } from './components/layouts/sidebar/sidebar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'sidebar', component:SidebarComponent}
+];
