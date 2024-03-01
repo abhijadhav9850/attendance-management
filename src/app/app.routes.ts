714301b61d8224e7182575b0b06b4c57da21f8f2
rudraBelwalkar <144021775+rudraBelwalkar@users.noreply.github.com>
2024-03-01 17:53:12 +0530

    added profile page router path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { DashboardLayoutComponent } from './pages/dashboard-layout/dashboard-layout.component';
+import { ProfileComponent } from './components/layouts/profile/profile.component';
 
 
 export const routes: Routes = [
 
     { path: '', pathMatch: 'full', redirectTo: 'dashboard-layout' },
     { path: 'dashboard-layout', component: DashboardLayoutComponent,children:[
-        {path:'dashboard', component: DashboardComponent}
+        {path:'dashboard', component: DashboardComponent},
+        {path:'profile',component: ProfileComponent}
     ] },
    
 
