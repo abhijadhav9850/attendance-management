1656e07188b5a267e82e7a612b0c96dbc1132016
rudraBelwalkar <144021775+rudraBelwalkar@users.noreply.github.com>
2024-03-01 18:58:04 +0530

    completed profile ui

--- src/app/components/layouts/profile/profile.component.ts
+++ src/app/components/layouts/profile/profile.component.ts
@@ -1,14 +1,23 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
 
+  name:string= 'Rudra Belwalkar'
   email:any = 'rudrabelwalkar09@gmail.com';
 
+  items:any[]=[
+    {class : 'bx bx-rocket',title:'Getting Started'},
+    {class : 'bx bx-question-mark',title:'Help'},
+    {class : 'bx bx-headphone',title:'Contact Support'},
+    {class : 'bx bx-headphone',title:"What's new"},
+    {class : 'bx bx-group',title:'Join the HR Community'},
+  ]
 }
