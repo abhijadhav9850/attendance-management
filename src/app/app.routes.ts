8ec2551a5a7f4998b26b906be4819fd9a7436891
shreekaudare <144114184+shreekaudare@users.noreply.github.com>
2024-03-04 23:21:49 +0530

    list-view page complit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,17 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { DashboardLayoutComponent } from './pages/dashboard-layout/dashboard-layout.component';
 import { ServicesPageComponent } from './pages/services-page/services-page.component';
+import { ListViewComponent } from './pages/list-view/list-view.component';
 
 
 export const routes: Routes = [
 
     { path: '', pathMatch: 'full', redirectTo: 'dashboard-layout' },
-    { path: 'dashboard-layout', component: DashboardLayoutComponent,children:[
-        {path:'dashboard', component: DashboardComponent},
-        {path:'Services', component:ServicesPageComponent}
-    ] },
+    {
+        path: 'dashboard-layout', component: DashboardLayoutComponent, children: [
+            { path: 'dashboard', component: DashboardComponent },
+            { path: 'Services', component: ServicesPageComponent },
+            { path: 'list-view', component: ListViewComponent }
+        ]
+    },
 ];
---
+++ src/app/pages/list-view/list-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-view.component.html',
+})
+export class ListViewComponent {
+
+}
