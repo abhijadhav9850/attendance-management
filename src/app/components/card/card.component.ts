4d9269fe907d86c333ed2fd950e06a78c0216037
Mayur Maruti Talekar <144109738+mayurtalekar@users.noreply.github.com>
2024-03-05 17:16:52 +0530

    timeer and card created complate

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -11,6 +11,20 @@ import { DatePipe } from '@angular/common';
 })
 export class CardComponent {  
   currentTime:any= new Date();
+  events:any=[
+    {img:"../../../assets/birthday-cake-birthday.svg",name:"Birthday"},
+    {img:"../../../assets/employee-icon-6.jpg",name:"New Hires"},
+    {img:"../../../assets/favorite-icon-png-8.jpg",name:"Favorites"},
+    {img:"../../../assets/quicklinks.jpg",name:"Quick Links"},
+    {img:"../../../assets/Announcements.png",name:"Announcements"},
+    {img:"../../../assets/Upcoming Holidays.png",name:"Upcoming Holidays"},
+  ]
 
+  leave:any=[
+    {leave:"Casual Leave", img:'../../../assets/casual.png',day1:1,day2:2},
+    {leave:"Leave Without Pay", img:'../../../assets/without.png',day1:1,day2:2},
+    {leave:"Sabbatical Leave", img:'../../../assets/Sabbatical.png',day1:1,day2:2},
+    {leave:"Sick Leave", img:'../../../assets/sick.png',day1:1,day2:2},
+  ]
   
 }
